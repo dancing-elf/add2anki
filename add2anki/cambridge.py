146b34c6f4cae98d8ca9b5795a1a8cce19c1de2b
Anton Saraev <antonysaraev@gmail.com>
2016-08-18 14:40:47 +0300

    Code cleanup

--- add2anki/cambridge.py
+++ add2anki/cambridge.py
@@ -82,27 +82,16 @@ _CAMBRIDGE_QUERY_URL = \
 
 def translate(word, src_lang, dst_lang):
     """fetch data from dictionary.cambridge.org in convenient format"""
-    url, bs = _query(word, src_lang, dst_lang)
-    dict_form = _extract_title(bs)
 
-    # check derivation form
-    if '“' in dict_form:
-        derived = re.search(r'“([^”]*)”', dict_form).group(0)
-        derived = derived[1:-1]
-        print('"{}" is derivation from "{}". Search it.'.format(word, derived))
-        url, bs = _query(derived, src_lang, dst_lang)
-        dict_form = _extract_title(bs)
+    url, bs, dict_form = _fetch_data(word, src_lang, dst_lang)
 
-    if not re.match(r'\A[\w]+\Z', dict_form):
-        raise Exception("Can't parse answer for {}".format(dict_form))
-
-    pos = []
+    pos_list = []
     for pos_html in bs.findAll(class_='posgram'):
-        pos.append(pos_html.span.text)
+        pos_list.append(pos_html.span.text)
 
     transcription = bs.find(class_='ipa').text
 
-    translations = []
+    trans_list = []
     for sense_block in bs.findAll(class_='sense-block'):
         if sense_block.find(class_='phrase-block'):
             continue
@@ -117,14 +106,15 @@ def translate(word, src_lang, dst_lang):
         example_block = sense_block.find(class_='examp')
         example = example_block.text if example_block else None
 
-        translations.append(CambridgeNote.CambridgeTranslation(
-            title, definition, trans, example))
+        trans_list.append(
+            CambridgeNote.CambridgeTranslation(
+                title, definition, trans, example))
 
-    return CambridgeNote(dict_form, pos, transcription, translations, url)
+    return CambridgeNote(dict_form, pos_list, transcription, trans_list, url)
 
 
 def to_html(note):
-    """ Convert CambridgeNote to html for Anki"""
+    """Convert CambridgeNote to html for Anki"""
     translations_html = ''
     for trans in note.translations:
         translations_html += '<div style="margin-top:0.5em">' \
@@ -134,30 +124,55 @@ def to_html(note):
                              '<div>{}</div>' \
                              '</div>'\
             .format(trans.title, trans.definition,
-                    _join_list(trans.translations), trans.example)
+                    _join(trans.translations), trans.example)
     return '<div style="color:#444;font-style:italic">{}</div>' \
            '<div style="color:#e84427">&frasl;{}&frasl;</div>' \
            '<div>{}</div>' \
            '<div style="margin-top:1.25em">' \
            '<a href="{}">See more on dictionary.cambridge.org</a>' \
            '</div>' \
-        .format(_join_list(note.pos_list), note.transcription,
+        .format(_join(note.pos_list), note.transcription,
                 translations_html, note.url)
 
 
-def _query(word, src_lang, dst_lang):
-    """ Make query for word's traslation"""
-    url = _CAMBRIDGE_QUERY_URL.format(src=src_lang, dst=dst_lang, word=word)
-    with urllib.request.urlopen(url) as response:
-        encoding = response.headers.get_content_charset()
-        html = response.read().decode(encoding)
-        return url, BeautifulSoup(html, 'html.parser')
+def _fetch_data(word, src_lang, dst_lang):
+    """Fetch data for parsing"""
+
+    def get_page_title(bs):
+        return bs.find(class_='di-title').text
+
+    def do_query(query_word):
+        query_url = _CAMBRIDGE_QUERY_URL.format(
+            src=src_lang, dst=dst_lang, word=query_word)
+        with urllib.request.urlopen(query_url) as response:
+            encoding = response.headers.get_content_charset()
+            html = response.read().decode(encoding)
+            return query_url, BeautifulSoup(html, 'html.parser')
+
+    def is_dictionary_form(string):
+        # dictionary form doesn't contains any special symbol.
+        # It's single word
+        return re.match(r'\A[\w]+\Z', string)
+
+    url, bs = do_query(word)
+    title = get_page_title(bs)
+
+    # check derivation form. If title contains special symbols
+    # then it is not dictionary form of query word and so we should
+    # extract it from page and fetch translation for that dictionary form
+    if not is_dictionary_form(title):
+        derived = re.search(r'“([^”]*)”', title).group(0)
+        derived = derived[1:-1]
+        print('"{}" is derivation from "{}". Search it.'.format(word, derived))
+        url, bs = do_query(derived)
+        title = get_page_title(bs)
+        # if we can't get dictionary form when we can't parse page
+        if not is_dictionary_form(title):
+            raise Exception("Can't find dictionary form for {}".format(title))
+
+    return url, bs, title
 
 
-def _extract_title(bs):
-    """ Extract title of page"""
-    return bs.find(class_='di-title').text
-
-
-def _join_list(string_list):
+def _join(string_list):
+    """Join string list with comma"""
     return ", ".join(string_list)
